17864f5650b1c173099e8e0667e97f9dcc2e5ee6
markkvdb <markvanderbroek@gmail.com>
2017-07-12 19:16:10 +0200

    Added user field to Students and Companies class for authentication.

--- companies/models.py
+++ companies/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from core.models import TimeStampedModel
+
 
 # Create your models here.
 class Company(TimeStampedModel):
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     description = models.CharField(max_length=1000)
--- students/models.py
+++ students/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Student(models.Model):
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=100)
     bio = models.CharField(max_length=1000)
